11e57a9eaefe38676d02ae9669809aa8be7a0185
hasanbektaş1 <hasanbktas1@gmail.com>
2023-12-14 10:37:12 +0300

    hasan text eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,8 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Container());
+        home: Scaffold(
+          body: Text("Hasan "),
+        ));
   }
 }
